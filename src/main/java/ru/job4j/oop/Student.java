3c56752b5786f9980326cee793bd5ba894b140c2
TheOneWhoseTimeHasCome <cadet211@gmail.com>
2022-12-28 14:05:11 +0500

    1.2. Вызов метода объекта [#166085 #375643]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -5,6 +5,7 @@ public class Student {
     public void music() {
         System.out.println("Tra tra tra");
     }
+
     public void song() {
         System.out.println("I believe I can fly");
     }
