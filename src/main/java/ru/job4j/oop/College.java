7eed79bc06799e5086eb8e58c443cc916691cb14
TheOneWhoseTimeHasCome <cadet211@gmail.com>
2023-01-10 17:43:17 +0500

    5.0. Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop;
+
+public class College {
+    public static void main(String[] args) {
+        Student freshman = new Freshman();
+        Object object = freshman;
+    }
+}
