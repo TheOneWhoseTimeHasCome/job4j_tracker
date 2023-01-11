e261e1ff11103d22af63ee160d75411eb805cd85
TheOneWhoseTimeHasCome <cadet211@gmail.com>
2023-01-11 16:42:35 +0500

    8. Input и полиморфизм. [#181094]

---
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,17 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StartUITest {
+    @Test
+    public void whenCreateItem() {
+        String[] answers = {"Replace thermal paste"};
+        Input input = new StubInput(answers);
+        Tracker tracker = new Tracker();
+        StartUI.createItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Replace thermal paste");
+        assertThat(created.getName()).isEqualTo(expected.getName());
+    }
+}
