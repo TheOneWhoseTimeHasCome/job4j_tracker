fa3eaa61d689a892e996355f0a601c0d3f901b65
TheOneWhoseTimeHasCome <cadet211@gmail.com>
2023-01-11 17:06:01 +0500

    9. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StartUITest {
@@ -13,5 +14,26 @@ public class StartUITest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Replace thermal paste");
         assertThat(created.getName()).isEqualTo(expected.getName());
+    }
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()), "edited item"};
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId())};
+        StartUI.deteleItem(new StubInput(answers), tracker);
+        assertThat(tracker.findById(item.getId())).isEqualTo(null);
     }
 }
