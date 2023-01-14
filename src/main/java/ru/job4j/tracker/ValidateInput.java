21a3547de8835c7d492805257c5a52402bde81bd
TheOneWhoseTimeHasCome <cadet211@gmail.com>
2023-01-14 18:50:09 +0500

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,6 +1,18 @@
 package ru.job4j.tracker;
 
 public class ValidateInput extends ConsoleInput {
+    private final Output out;
+    private final Input in;
+
+    public ValidateInput(Output out, Input input) {
+        this.out = out;
+        this.in = input;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return in.askStr(question);
+    }
 
     @Override
     public int askInt(String question) {
