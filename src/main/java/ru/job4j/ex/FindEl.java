e2617705c900b8ab25573a06fb23b538cfab4ae0
TheOneWhoseTimeHasCome <cadet211@gmail.com>
2023-01-14 09:05:38 +0500

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Элемент не найден");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            indexOf(new String[]{"qqq", "aaa", "ooo"}, "eee");
+        } catch (ElementNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
