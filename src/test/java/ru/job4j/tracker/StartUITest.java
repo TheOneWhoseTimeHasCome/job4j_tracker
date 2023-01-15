2d49cd50f38a484667f1397314addd49f6d88abf
TheOneWhoseTimeHasCome <cadet211@gmail.com>
2023-01-15 14:12:35 +0500

    1.1. Тесты на StartUI. [#223313 #381097]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -216,7 +216,7 @@ public class StartUITest {
     public void whenInvalidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"one", "1"}
+                new String[]{"one", "1"}
         );
         ValidateInput input = new ValidateInput(out, in);
         int selected = input.askInt("Enter menu:");
