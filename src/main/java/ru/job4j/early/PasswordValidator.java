08699f5ad92275acd5aa55afcaee9062afdde6cd
TheOneWhoseTimeHasCome <cadet211@gmail.com>
2023-01-17 14:15:16 +0500

    4. Принципы раннего возврата и охранных выражений [#504875 #381528]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -1,6 +1,7 @@
 package ru.job4j.early;
 
 public class PasswordValidator {
+    @SuppressWarnings("checkstyle:SimplifyBooleanExpression")
     public static String validate(String password) {
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
@@ -8,30 +9,38 @@ public class PasswordValidator {
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-        int countUpper = 0;
-        int countLower = 0;
-        int countDigit = 0;
-        int countSpecial = 0;
+        boolean haveUpper = false;
+        boolean haveLower = false;
+        boolean haveDigit = false;
+        boolean haveSpecial = false;
         for (int i = 0; i < password.length(); i++) {
-            countUpper = Character.isUpperCase(password.charAt(i)) ? ++countUpper : countUpper;
-            countLower = Character.isLowerCase(password.charAt(i)) ? ++countLower : countLower;
-            countDigit = Character.isDigit(password.charAt(i)) ? ++countDigit : countDigit;
-            countSpecial = !Character.isLetterOrDigit(password.charAt(i)) ? ++countSpecial : countSpecial;
-            if (countSpecial > 0 && countDigit > 0 && countLower > 0 && countUpper > 0) {
+            if (Character.isUpperCase(password.charAt(i))) {
+                haveUpper = true;
+            }
+            if (Character.isLowerCase(password.charAt(i))) {
+                haveLower = true;
+            }
+            if (Character.isDigit(password.charAt(i))) {
+                haveDigit = true;
+            }
+            if (!Character.isLetterOrDigit(password.charAt(i))) {
+                haveSpecial = true;
+            }
+            if (haveSpecial && haveUpper && haveDigit && haveLower) {
                 break;
             }
-            if (i == password.length() - 1 && countUpper == 0) {
-                throw new IllegalArgumentException("Password should contain at least one uppercase letter");
-            }
-            if (i == password.length() - 1 && countLower == 0) {
-                throw new IllegalArgumentException("Password should contain at least one lowercase letter");
-            }
-            if (i == password.length() - 1 && countDigit == 0) {
-                throw new IllegalArgumentException("Password should contain at least one figure");
-            }
-            if (i == password.length() - 1 && countSpecial == 0) {
-                throw new IllegalArgumentException("Password should contain at least one special symbol");
-            }
+        }
+        if (!haveUpper) {
+            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
+        }
+        if (!haveLower) {
+            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
+        }
+        if (!haveDigit) {
+            throw new IllegalArgumentException("Password should contain at least one figure");
+        }
+        if (!haveSpecial) {
+            throw new IllegalArgumentException("Password should contain at least one special symbol");
         }
         String[] words = {"qwerty", "12345", "password", "admin", "user"};
             for (String word : words) {
